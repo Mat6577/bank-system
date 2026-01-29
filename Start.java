1779c429bb7527a22c4455dd35128488b59c00ae
Tebello <Tebellomohohoma03@gmail.com>
2026-01-29 13:17:53 +0200

    Created basic finance operations to the project

---
+++ Start.java
@@ -0,0 +1,8 @@
+package project4;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Project 4!");
+        // Additional startup logic can be added here
+    }
+}
